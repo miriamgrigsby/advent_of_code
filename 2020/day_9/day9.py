fc15f3189e1d1ed0e4b6d546c88f3266ad476d43
Miriam Grigsby <miriamgrigsby@gmail.com>
2023-01-13 12:52:58 -0700

    Feat: 2020 day9 part 2

--- 2020/day_9/day9.py
+++ 2020/day_9/day9.py
@@ -60,3 +60,47 @@ def solve_day9_part1(input: List[int], preamble: int) -> int:
 def test_solve_day9_part1(input: List[int], preamble: int, expected_result):
     result = solve_day9_part1(input, preamble)
     assert result == expected_result
+
+def solve_day9_part2(num_array: List[int], incorrect_number: int,) -> int:
+    for start_index in range(len(num_array)):
+        for end_index in range(len(num_array) - start_index):
+            if sum(num_array[start_index:start_index+end_index]) == incorrect_number:
+                return min(num_array[start_index:start_index+end_index]) + max(num_array[start_index:start_index+end_index])
+    return 0
+
+@pytest.mark.parametrize("num_array,expected_result,preamble", [
+    (
+      [
+                35,
+                20,
+                15,
+                25,
+                47,
+                40,
+                62,
+                55,
+                65,
+                95,
+                102,
+                117,
+                150,
+                182,
+                127,
+                219,
+                299,
+                277,
+                309,
+                576
+            ],
+            62 ,
+            5
+    ),
+    (
+        parsed_data,
+        3340942, 25
+    )
+])
+def test_solve_day9_part2(num_array: List[int], expected_result, preamble):
+    incorrect_number = solve_day9_part1(num_array, preamble)
+    result = solve_day9_part2(num_array, incorrect_number)
+    assert result == expected_result
